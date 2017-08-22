e58819652fbe12895468e205e753822535436a4c
hstdt <hstdt@163.com>
2017-08-22 13:46:39 +0800

    feature: resize image attachment display size
    Resize to make image attachment has the same width with line's width without changing self.image.bounds

---
+++ RichTextEditor/Source/RichTextImageAttachment.h
@@ -0,0 +1,13 @@
+//
+//  RichTextImageAttachment.h
+//  RichTextEditor
+//
+//  Created by hstdt on 2017/8/22.
+//  Copyright © 2017年 Aryan Ghassemi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RichTextImageAttachment : NSTextAttachment
+
+@end
